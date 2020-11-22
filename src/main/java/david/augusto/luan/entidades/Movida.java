036df52b7f762ee4def63978652faa77e8591161
luan-alencar <luanalencar134@gmail.com>
2020-11-22 09:46:47 -0300

    voltando método ao seu estado inicial

--- src/main/java/david/augusto/luan/entidades/Movida.java
+++ src/main/java/david/augusto/luan/entidades/Movida.java
@@ -3,9 +3,7 @@ package david.augusto.luan.entidades;
 public class Movida extends Locadora {
 
 	public double alugarCarro(Carro carro, Data dataInicio, Data dataFim) {
-		carro.isAlugado();
-		double total = 30;
-		return (total * 0.10) + 30;
+		return alugarCarro(carro, dataInicio, dataFim);
 	}
 
 }
