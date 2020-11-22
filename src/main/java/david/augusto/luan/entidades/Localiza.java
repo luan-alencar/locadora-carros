f832fa0c66645ec3670190395ff35b820e716c40
luan-alencar <luanalencar134@gmail.com>
2020-11-22 09:51:56 -0300

    adicionando condição para que se o carro já estiver alugado, lance a exceção CarroAlugadoException

--- src/main/java/david/augusto/luan/entidades/Localiza.java
+++ src/main/java/david/augusto/luan/entidades/Localiza.java
@@ -2,16 +2,15 @@ package david.augusto.luan.entidades;
 
 import david.augusto.luan.exceptions.CarroAlugadoException;
 
-public class Localiza extends Locadora{
+public class Localiza extends Locadora {
 
 	public double alugarCarro(Carro carro, Data dataInicio, Data dataFim) throws CarroAlugadoException {
-		if(carro.isAlugado() == true) {
-			throw new  CarroAlugadoException();
+		if (carro.isAlugado() == true) {
+			throw new CarroAlugadoException();
 		}
 		carro.isAlugado();
 		double total = 30;
 		return (total * 0.20) + 30;
 	}
 
-	
 }
