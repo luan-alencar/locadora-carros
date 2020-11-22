a9acb1935da3838fa5c25fed7a41fed04c9fc44c
luan-alencar <luanalencar134@gmail.com>
2020-11-22 09:46:10 -0300

    implementando CarroAlugadoException, caso aquele carro já tenha sido alugado

--- src/main/java/david/augusto/luan/entidades/Localiza.java
+++ src/main/java/david/augusto/luan/entidades/Localiza.java
@@ -1,10 +1,16 @@
 package david.augusto.luan.entidades;
 
+import david.augusto.luan.exceptions.CarroAlugadoException;
+
 public class Localiza extends Locadora{
 
-	public double alugarCarro(Carro carro, Data dataInicio, Data dataFim) {
-		// TODO Auto-generated method stub
-		return alugarCarro(carro, dataInicio, dataFim);
+	public double alugarCarro(Carro carro, Data dataInicio, Data dataFim) throws CarroAlugadoException {
+		if(carro.isAlugado() == true) {
+			throw new  CarroAlugadoException();
+		}
+		carro.isAlugado();
+		double total = 30;
+		return (total * 0.20) + 30;
 	}
 
 	
