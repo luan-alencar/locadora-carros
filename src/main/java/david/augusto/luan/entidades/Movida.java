736eb954d9b1d5b5250bc43da89bb09d789af002
luan-alencar <luanalencar134@gmail.com>
2020-11-22 09:34:23 -0300

    modificando alugarCarro

--- src/main/java/david/augusto/luan/entidades/Movida.java
+++ src/main/java/david/augusto/luan/entidades/Movida.java
@@ -3,7 +3,9 @@ package david.augusto.luan.entidades;
 public class Movida extends Locadora {
 
 	public double alugarCarro(Carro carro, Data dataInicio, Data dataFim) {
-		return alugarCarro(carro, dataInicio, dataFim);
+		carro.isAlugado();
+		double total = 30;
+		return (total * 0.10) + 30;
 	}
 
 }
